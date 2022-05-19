7afa7a694762ed6961582a978a624804f6dac116
Jorge Jara <jorgejarainostroza@gmail.com>
2022-05-19 11:46:36 -0400

    neovim: Override Vue indent settings

--- nvim/lua/syntax.lua
+++ nvim/lua/syntax.lua
@@ -3,14 +3,15 @@ vim.cmd(
     "autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()")
 
 -- Format options
-vim.opt.formatoptions = vim.opt.formatoptions + {
-    t = true,
-    c = true,
-    r = true,
-    q = true,
-    n = true,
-    j = true
-}
+vim.opt.formatoptions = vim.opt.formatoptions +
+                            {
+        t = true,
+        c = true,
+        r = true,
+        q = true,
+        n = true,
+        j = true
+    }
 
 require('Comment').setup()
 
@@ -19,9 +20,8 @@ vim.cmd('autocmd Filetype javascript setlocal ts=2 sw=2 sts=0 expandtab')
 vim.cmd('autocmd Filetype javascriptreact setlocal ts=2 sw=2 sts=0 expandtab')
 vim.cmd('autocmd Filetype typescript setlocal ts=2 sw=2 sts=0 expandtab')
 vim.cmd('autocmd Filetype typescriptreact setlocal ts=2 sw=2 sts=0 expandtab')
+vim.cmd('autocmd Filetype vue setlocal ts=2 sw=2 sts=0 expandtab')
 vim.cmd('autocmd Filetype json setlocal ts=2 sw=2 sts=0 expandtab')
 
 -- nvim-autopairs
-require('nvim-autopairs').setup({
-    enable_check_bracket_line = false
-})
+require('nvim-autopairs').setup({enable_check_bracket_line = false})
