d62b86e3e0de19dae9972608b6b9ccf382927ff7
Jorge Jara <jorgejarainostroza@gmail.com>
2023-11-11 12:52:07 -0300

    neovim: Migrate indent-blankline settings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -137,10 +137,7 @@ require('packer').startup(function(use)
     use {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
-            require('indent_blankline').setup({
-                show_current_context = true,
-                show_current_context_start = true
-            })
+            require('ibl').setup({})
         end
     }
     use {
