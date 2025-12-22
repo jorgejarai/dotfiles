86cf5ddd0913a7be92bb57be24b6f6867e13d159
Jorge Jara <jorgejarainostroza@gmail.com>
2025-12-22 15:35:35 -0300

    vscode(nocebo): enable VS Code server

--- nixos/hosts/nocebo/home-manager/default.nix
+++ nixos/hosts/nocebo/home-manager/default.nix
@@ -6,5 +6,12 @@
 
     ./packages.nix
     ./rcfiles.nix
+
+    "${fetchTarball {
+      url = https://github.com/msteen/nixos-vscode-server/tarball/master;
+      sha256 = "1rdn70jrg5mxmkkrpy2xk8lydmlc707sk0zb35426v1yxxka10by";
+    }}/modules/vscode-server/home.nix"
   ];
+
+  services.vscode-server.enable = true;
 }
